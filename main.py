083c0b6311279fed0fbe5149005e740e490b10ec
Yasser Sinjab <ysinjab@gmail.com>
2019-06-16 19:41:47 +0300

    - add pycode style
    - add pep8 code style and enhance pep8 for scripts files

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """Brisk main script
 
-This is the script of brisk. A program that always happy and say only hello and only hello.
-Unfortunately it works for 1 minute only so run it again if you like it.
+This is the script of brisk. A program that always happy and say only \n
+hello and only hello.Unfortunately it works for 1 minute only so run \n
+it again if you like it.
 
 Example:
     $ python3 main.py
@@ -10,8 +11,9 @@ This script contains the following functions:
     * say_hello()
 
 """
+
 import time
-import os 
+import os
 
 TIME_TO_SLEEP = 5
 TOTAL_TIME_TO_WORK = 60
@@ -24,6 +26,7 @@ def say_hello():
         print('hello')
         time.sleep(TIME_TO_SLEEP)
         time_elapsed += TIME_TO_SLEEP
+
 
 if __name__ == '__main__':
     say_hello()
--- test_say_hello.py
+++ test_say_hello.py
@@ -8,9 +8,10 @@ To run:
 import main
 from main import say_hello
 
+
 def test_say_hello_does_not_print_bye(capsys, monkeypatch):
     monkeypatch.setattr(main, 'TOTAL_TIME_TO_WORK', 2, raising=True)
     monkeypatch.setattr(main, 'TIME_TO_SLEEP', 1, raising=True)
     say_hello()
     captured = capsys.readouterr()
-    assert 'bye' not in  captured.out
+    assert 'bye' not in captured.out
